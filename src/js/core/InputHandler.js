028117bf97f6afbbd1da4abce76dd20661e0fc2d
Aurelian <aurelians@libero.it>
2018-12-07 05:50:08 +0000

    Updated the keyCode to the object

--- src/js/core/InputHandler.js
+++ src/js/core/InputHandler.js
@@ -13,18 +13,15 @@ export default class InputHandler {
 
         document.addEventListener("keydown", event => {
             console.log("Key down " + event.keyCode);
-            if(event.keyCode == 39) {
-                event.preventDefault();
-                pressed.rightPressed = true;
-            } else if(event.keyCode == 37) {
-                event.preventDefault();
-                pressed.leftPressed = true;
-            } if(event.keyCode == 40) {
-                event.preventDefault();
-                pressed.downPressed = true;
-            } else if(event.keyCode == 38) {
-                event.preventDefault();
+
+            if(event.keyCode == keys.arrowUp) {
                 pressed.upPressed = true;
+            } else if(event.keyCode == keys.arrowRight) {
+                pressed.rightPressed = true;
+            } else if(event.keyCode == keys.arrowDown) {
+                pressed.downPressed = true;
+            } else if(event.keyCode == keys.arrowLeft) {
+                pressed.leftPressed = true;
             }
         });  
 
